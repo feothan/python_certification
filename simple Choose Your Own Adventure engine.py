f44e89877be8ce6b41fa28f4cd32eca105979c70
Pete Williams <mrpetewilliams@comcast.net>
2025-07-09 23:24:43 -0400

    Here's a simple, entirely text-based implementation of the Choose Your Own Adventure framework. Pretty pleased with how well this works, and I think it's easy to write a game in (!).

---
+++ simple Choose Your Own Adventure engine.py
@@ -0,0 +1,74 @@
+import subprocess
+game_title_page = "\n\nWelcome to\nBusScape!\n"
+game_not_over = True
+page = 1
+clear_screen_after_each_page = False
+
+story_pages = {
+    1: {
+        'description': "You're on the bus. Your stop's ahead.",
+        'choices': {
+            1: {'choice': "Pull the chord.", 'go_to_page': 2},
+            2: {'choice': "Look at your phone.",'go_to_page': 3}
+        }
+    },
+    2: {
+        'description': "The bus driver opens the doors, but there's a dude wearing a rabbit mascot outfit in your way!",
+        'choices': {
+            1: {'choice': "Push the rabbit dude out of the way.", 'go_to_page': 4},
+            2: {'choice': "Ask him (or her) politely to step aside.", 'go_to_page': 5},
+            3: {'choice': "Try to climb out the window.", 'go_to_page': 7}
+        }
+    },
+    3: {
+        'description': "The bus driver gets distracted by the very funny video on your phone and crashes the bus. You die.",
+        'choices': None
+    },
+    4: {
+        'description': "Rabbit dude is a super-tough dudette, and she wipes the floor with you.",
+        'choices': None
+    },
+    5: {
+        'description': "In rabbit language, your polite words sound very angry.",
+        'choices': {
+            1: {'choice': "More.", 'go_to_page': 6}
+        }
+    },
+    6: {
+        'description': "The rabbit folds you up into a tiny cube and puts you in her purse.",
+        'choices': None
+    },
+    7: {
+        'description': "You climb out the window to freedom. And terrible traffic injury.",
+        'choices': None
+    }
+}
+
+print(game_title_page)
+while game_not_over:
+    if clear_screen_after_each_page:
+        subprocess.run("cls" if subprocess.os.name == "nt" else "clear", shell=True)
+
+    print(story_pages[page]['description'])
+
+    choices = story_pages[page]['choices']
+    if not choices:
+        print("\nTHE END")
+        game_not_over = False
+        break
+
+    print("Choices:")
+    for num, choice_info in choices.items():
+        print(f"{num}: {choice_info['choice']}")
+
+    while True:
+        try:
+            decision = int(input("\nWhat do you do? "))
+            if decision in choices:
+                page = choices[decision]['go_to_page']
+                print('\n')
+                break  # break out of input loop, not game loop
+            else:
+                print("Invalid choice. Try again.")
+        except ValueError:
+            print("Please enter a number.")
